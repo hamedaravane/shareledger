a9dc34832abeef9cee60a25609ee9b49844936a7
Hamed Arghavan <hamedaravane@gmail.com>
2024-10-28 19:12:48 +0330

    edit the bottom-nav.component.ts

--- src/shared/component/bottom-nav/bottom-nav.component.ts
+++ src/shared/component/bottom-nav/bottom-nav.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, Input } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { NgClass } from '@angular/common';
 
 interface Navigation {
   icon: string;
@@ -15,38 +17,57 @@ interface Navigation {
       position: fixed;
       bottom: 0;
     }
+
+    nav {
+      padding-bottom: calc(1rem + env(safe-area-inset-bottom));
+      padding-top: 0.5rem;
+    }
   `,
   template: `
-    <nav class="bg-neutral-300">
-      <div class="flex w-full pb-4 justify-evenly">
-        @for (navigation of navigations; track $index) {
-          <button [routerLink]="navigation.path">
-            <i class="fa-solid {{ navigation.icon }}"></i>
-            <span>{{ navigation.label }}</span>
-          </button>
-        }
-      </div>
+    <nav class="bg-neutral-200 flex justify-evenly">
+      @for (navigation of navigations; track $index) {
+        <button
+          class="text-neutral-500 text-xs"
+          aria-hidden="true"
+          [routerLink]="navigation.path"
+          routerLinkActive="active"
+          [tabindex]="$index">
+          <mat-icon
+            [ngClass]="{ 'drop-shadow-md text-gray-600': isActive(navigation.path) }"
+            [fontIcon]="navigation.icon"></mat-icon>
+          <label
+            class="block"
+            [ngClass]="{ 'drop-shadow-md text-gray-600': isActive(navigation.path) }">
+            {{ navigation.label }}
+          </label>
+        </button>
+      }
     </nav>
   `,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon, RouterLinkActive, NgClass],
   standalone: true,
 })
 export class BottomNavComponent {
   @Input() navigations: Navigation[] = [
     {
-      icon: 'fa-dashboard',
+      icon: 'dashboard',
       label: 'Dashboard',
-      path: '/',
+      path: '/app',
     },
     {
-      icon: 'fa-setting',
-      label: 'Setting',
-      path: '/',
+      icon: 'receipt',
+      label: 'Transactions',
+      path: '/transactions',
     },
     {
-      icon: 'fa-window',
-      label: 'Window',
-      path: '/',
+      icon: 'account_circle',
+      label: 'Profile',
+      path: '/profile',
     },
   ];
+  private readonly router = inject(Router);
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
 }
